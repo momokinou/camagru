02dd39c13920f0999770054a17095a4fa26bc293
momokinou <31689903+momokinou@users.noreply.github.com>
2019-03-19 17:50:04 +0100

    Add files via upload

---
+++ views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="../assets/css/header.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camagru</title>
+</head>
+<body>
+	<ul class="topnav">
+		<li><a href="">Ajouter une photo</a></li>
+		<li class= "camagru"><a href="">Camagru</a></li>
+		<li><a href="">Mon compte</a></li>
+	</ul>
+</body>
+</html>
